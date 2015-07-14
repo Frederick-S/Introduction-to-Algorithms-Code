eb1cb7f3336741df4bd01034ee0176c1ea9ea219
Frederick-S <mao_xiaodan@hotmail.com>
2015-07-14 03:16:53 +0000

    Add insertion sort

--- tests/demo_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src import demo
-
-class TestStringMethods(unittest.TestCase):
-    def test_sum(self):
-        self.assertTrue(5, demo.sum(1, 4))
-
-if __name__ == '__main__':
-    unittest.main()
