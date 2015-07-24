862226e31b707b7a2837695381fa1636b9dbc8d4
Frederick-S <mao_xiaodan@hotmail.com>
2015-07-24 08:40:31 +0000

    Add selection sort

--- tests/selection_sort_test.py
+++ tests/selection_sort_test.py
@@ -1,26 +1,26 @@
 import unittest
-from src.insertion_sort import insertion_sort
+from src.selection_sort import selection_sort
 
-class TestInsertionSort(unittest.TestCase):
+class TestSelectionSort(unittest.TestCase):
     def test_insertion_sort(self):
         numbers = []
-        insertion_sort(numbers)
+        selection_sort(numbers)
         self.assertSequenceEqual([], numbers)
         
         numbers = [1]
-        insertion_sort(numbers)
+        selection_sort(numbers)
         self.assertSequenceEqual([1], numbers)
         
         numbers = [31, 41, 59, 26, 41, 58]
-        insertion_sort(numbers)
+        selection_sort(numbers)
         self.assertSequenceEqual([26, 31, 41, 41, 58, 59], numbers)
         
         numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        insertion_sort(numbers)
+        selection_sort(numbers)
         self.assertSequenceEqual([1, 2, 3, 4, 5, 6, 7, 8, 9], numbers)
         
         numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-        insertion_sort(numbers)
+        selection_sort(numbers)
         self.assertSequenceEqual([1, 2, 3, 4, 5, 6, 7, 8, 9], numbers)
 
 if __name__ == '__main__':
