68b649c0f0a52998b01b2334c4d54c84e57da8ad
Frederick-S <mao_xiaodan@hotmail.com>
2016-04-04 04:54:54 +0000

    Add random number

---
+++ tests/chapter_5/random_number_test.py
@@ -0,0 +1,11 @@
+import unittest
+from src.chapter_5.random_number import random_number
+
+
+class TestRandomNumber(unittest.TestCase):
+    def test_random_number(self):
+        self.assertEqual(5, random_number(5, 5))
+        self.assertEqual(True, 1 <= random_number(1, 10) <= 10)
+
+if __name__ == '__main__':
+    unittest.main()
