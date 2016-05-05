acecfef96a532730c21439fc0236e2ff38fca72f
Frederick-S <mao_xiaodan@hotmail.com>
2016-05-05 07:30:16 +0000

    Add hybrid quicksort

---
+++ src/chapter_7/hybrid_quick_sort.py
@@ -0,0 +1,28 @@
+from ..chapter_2.insertion_sort import insertion_sort
+
+
+def hybrid_quick_sort(numbers, k):
+    quick_sort_internal(numbers, 0, len(numbers) - 1, k)
+    insertion_sort(numbers)
+
+
+def quick_sort_internal(numbers, p, r, k):
+    if p < r and r - p + 1 < k:
+        q = partition(numbers, p, r)
+
+        quick_sort_internal(numbers, p, q - 1, k)
+        quick_sort_internal(numbers, q + 1, r, k)
+
+
+def partition(numbers, p, r):
+    x = numbers[r]
+    i = p - 1
+
+    for j in range(p, r):
+        if numbers[j] <= x:
+            i += 1
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+    numbers[i + 1], numbers[r] = numbers[r], numbers[i + 1]
+
+    return i + 1
---
+++ tests/chapter_7/hybrid_quick_sort_test.py
@@ -0,0 +1,16 @@
+import unittest
+from random import randint
+from src.chapter_7.hybrid_quick_sort import hybrid_quick_sort
+
+
+class TestHybridQuickSort(unittest.TestCase):
+    def test_hybrid_quick_sort(self):
+        numbers = [randint(1, 1000) for i in range(1000)]
+        numbers_sorted = numbers[:]
+        numbers_sorted.sort()
+
+        hybrid_quick_sort(numbers, 50)
+        self.assertSequenceEqual(numbers_sorted, numbers)
+
+if __name__ == '__main__':
+    unittest.main()
