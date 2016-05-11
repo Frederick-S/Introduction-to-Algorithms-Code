93b79d3a644f3bdfb60cf0bd87662438e13b2777
Frederick-S <mao_xiaodan@hotmail.com>
2016-05-11 02:11:10 +0000

    Update radix sort

--- src/chapter_8/radix_sort.py
+++ src/chapter_8/radix_sort.py
@@ -1,10 +1,13 @@
+import math
+
+
 def radix_sort(numbers):
     length = len(numbers)
 
     if length == 0:
         return numbers
 
-    d = len(str(max(numbers)))
+    d = get_number_length(max(numbers))
 
     for i in range(1, d + 1):
         sort_on_digit(numbers, i)
@@ -31,7 +34,14 @@ def sort_on_digit(numbers, i):
 
 
 def get_digit(number, i):
-    if i > len(str(number)):
+    if i > get_number_length(number):
         return 0
     else:
         return int(str(number)[-i])
+
+
+def get_number_length(number):
+    if number == 0:
+        return 1
+    else:
+        return int(math.log(number, 10)) + 1
