05409ff59973882bfe77f9221f584888e2fb2cad
Frederick-S <mao_xiaodan@hotmail.com>
2016-05-11 01:44:33 +0000

    Update test

--- tests/chapter_8/radix_sort_test.py
+++ tests/chapter_8/radix_sort_test.py
@@ -12,9 +12,9 @@ class TestRadixSort(unittest.TestCase):
         radix_sort(numbers)
         self.assertSequenceEqual([1], numbers)
 
-        numbers = [31, 41, 59, 26, 41, 58]
+        numbers = [31, 41, 59, 26, 41, 58, 100]
         radix_sort(numbers)
-        self.assertSequenceEqual([26, 31, 41, 41, 58, 59], numbers)
+        self.assertSequenceEqual([26, 31, 41, 41, 58, 59, 100], numbers)
 
         numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         radix_sort(numbers)
