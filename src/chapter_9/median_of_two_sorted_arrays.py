256ed93f92f99db0353b6e3def03f8db833d501c
Frederick-S <mao_xiaodan@hotmail.com>
2016-05-29 08:21:11 +0000

    Update

--- src/chapter_9/median_of_two_sorted_arrays.py
+++ src/chapter_9/median_of_two_sorted_arrays.py
@@ -7,21 +7,20 @@ def median_of_two_sorted_arrays(x, y):
 
 
 def median_of_two_sorted_arrays_internal(x, px, rx, y, py, ry):
-    if rx - px == 0 and ry - py == 0:
-        return min(x[px], y[py])
-    elif px > rx:
-        return y[py]
-    elif py > ry:
-        return x[px]
+    if rx - px == 1:
+        if x[px] <= y[py]:
+            return min(x[rx], y[py])
+        else:
+            return min(x[px], y[ry])
 
     median_index_of_x = (px + rx) // 2
     median_index_of_y = (py + ry) // 2
 
     if x[median_index_of_x] < y[median_index_of_y]:
         return median_of_two_sorted_arrays_internal(
-            x, median_index_of_x + 1, rx, y, py, median_index_of_y - 1)
+            x, median_index_of_x, rx, y, py, median_index_of_y)
     elif x[median_index_of_x] > y[median_index_of_y]:
         return median_of_two_sorted_arrays_internal(
-            x, px, median_index_of_x - 1, y, median_index_of_y + 1, ry)
+            x, px, median_index_of_x, y, median_index_of_y, ry)
     else:
         return x[median_index_of_x]
