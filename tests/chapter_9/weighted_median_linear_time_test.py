2c82889bced41476c79450e675ceb2fc83272cb3
Frederick-S <mao_xiaodan@hotmail.com>
2016-06-01 01:27:33 +0000

    Update test

--- tests/chapter_9/weighted_median_linear_time_test.py
+++ tests/chapter_9/weighted_median_linear_time_test.py
@@ -17,6 +17,11 @@ class TestWeightedMedianLinearTime(unittest.TestCase):
         elements = create_elements(values, weights)
         self.assertEqual(1, weighted_median_linear_time(elements).get('value'))
 
+        values = [1, 2, 3]
+        weights = [0.1, 0.1, 0.8]
+        elements = create_elements(values, weights)
+        self.assertEqual(3, weighted_median_linear_time(elements).get('value'))
+
         values = [1, 2, 3, 4, 5, 6, 7]
         weights = [0.1, 0.35, 0.1, 0.05, 0.15, 0.05, 0.2]
         elements = create_elements(values, weights)
