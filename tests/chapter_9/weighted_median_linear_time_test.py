0b4e8f046c6c7f25a7294eb8ee99bd3006969111
Frederick-S <mao_xiaodan@hotmail.com>
2016-06-01 01:17:27 +0000

    Weighted median in linear time

--- tests/chapter_9/weighted_median_linear_time_test.py
+++ tests/chapter_9/weighted_median_linear_time_test.py
@@ -1,12 +1,22 @@
 import unittest
 import random
+from .weighted_median_test import create_elements
 from src.chapter_9.weighted_median_linear_time \
     import weighted_median_linear_time
 
 
 class TestWeightedMedianLinearTime(unittest.TestCase):
     def test_weighted_median_linear_time(self):
-        pass
+        values = [1, 2, 3, 4, 5, 6, 7]
+        weights = [0.1, 0.35, 0.1, 0.05, 0.15, 0.05, 0.2]
+        elements = create_elements(values, weights)
+        self.assertEqual(3, weighted_median_linear_time(elements).get('value'))
+
+        values = [0.1 for i in range(10)]
+        weights = [0.1 for i in range(10)]
+        elements = create_elements(values, weights)
+        self.assertEqual(
+            0.1, weighted_median_linear_time(elements).get('value'))
 
 if __name__ == '__main__':
     unittest.main()
