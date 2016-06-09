7422999976ec06c9a370df2063e94cfdd6c07e43
Frederick-S <mao_xiaodan@hotmail.com>
2016-06-09 02:12:58 +0000

    Add doubly linked list

--- src/chapter_10/doubly_linked_list.py
+++ src/chapter_10/doubly_linked_list.py
@@ -1,2 +1,43 @@
+class DoublyLinkedNode(object):
+    def __init__(self, key):
+        self.key = key
+        self.prev = None
+        self.next = None
+
+
 class DoublyLinkedList(object):
-    pass
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def search(self, key):
+        node = self.head
+
+        while node is not None and node.key != key:
+            node = node.next
+
+        return node
+
+    def insert(self, key):
+        new = DoublyLinkedNode(key)
+
+        if self.head is None:
+            self.head = new
+        else:
+            new.next = self.head
+            self.head.prev = new
+            self.head = new
+
+    def delete(self, key):
+        current = self.head
+
+        while current is not None and current.key != key:
+            current = current.next
+
+        if current is not None:
+            current.prev.next = current.next
+
+            if current.next is not None:
+                current.next.prev = current.prev
--- tests/chapter_10/doubly_linked_list_test.py
+++ tests/chapter_10/doubly_linked_list_test.py
@@ -4,7 +4,19 @@ from src.chapter_10.doubly_linked_list import DoublyLinkedList
 
 class TestDoublyLinkedList(unittest.TestCase):
     def doubly_linked_list(self):
-        pass
+        doubly_linked_list = DoublyLinkedList()
+        doubly_linked_list.insert(1)
+        doubly_linked_list.insert(2)
+        doubly_linked_list.insert(3)
+        self.assertEqual(None, doubly_linked_list.search(4))
+        self.assertEqual(1, doubly_linked_list.search(1).key)
+
+        doubly_linked_list.delete(1)
+        self.assertEqual(None, doubly_linked_list.search(1))
+
+        doubly_linked_list.delete(2)
+        doubly_linked_list.delete(3)
+        self.assertTrue(doubly_linked_list.is_empty())
 
 if __name__ == '__main__':
     unittest.main()
