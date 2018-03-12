fcfbf6182d93e0842186c0266244a0f95ff05355
Frederick-S <mao_xiaodan@hotmail.com>
2018-03-12 23:12:44 +0800

    Update test

--- tests/chapter_10/dict_using_singly_linked_list_test.py
+++ tests/chapter_10/dict_using_singly_linked_list_test.py
@@ -10,6 +10,14 @@ class TestDictUsingSinglyLinkedList(unittest.TestCase):
         self.assertEqual(1, dict_using_singly_linked_list.search(1))
         self.assertEqual(None, dict_using_singly_linked_list.search(2))
 
+        dict_using_singly_linked_list.insert(2, 2)
+        with self.assertRaises(Exception):
+            dict_using_singly_linked_list.insert(2, 2)
+        
+        dict_using_singly_linked_list.delete(2)
+        with self.assertRaises(Exception):
+            dict_using_singly_linked_list.delete(2)
+
 
 if __name__ == '__main__':
     unittest.main()
