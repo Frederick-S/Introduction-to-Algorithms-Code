56736f3c4cef045f01e89a601ab23c078697fa48
Frederick-S <mao_xiaodan@hotmail.com>
2018-03-15 20:39:03 +0800

    Update 10.2-5

--- src/chapter_10/dict_using_singly_linked_circular_list.py
+++ src/chapter_10/dict_using_singly_linked_circular_list.py
@@ -1,9 +1,9 @@
-from .singly_linked_list import SinglyLinkedList
+from .singly_linked_circular_list import SinglyLinkedCircularList
 
 
-class DictUsingSinglyLinkedList(object):
+class DictUsingSinglyLinkedCircularList(object):
     def __init__(self):
-        self.singly_linked_list = SinglyLinkedList()
+        self.singly_linked_circular_list = SinglyLinkedCircularList()
 
     def insert(self, key, value):
         dict_value = self.search(key)
@@ -11,17 +11,17 @@ class DictUsingSinglyLinkedList(object):
         if dict_value:
             raise Exception('The specified key already exists')
         else:
-            self.singly_linked_list.insert(key, value)
+            self.singly_linked_circular_list.insert(key, value)
 
     def delete(self, key):
         value = self.search(key)
 
         if value:
-            self.singly_linked_list.delete(key)
+            self.singly_linked_circular_list.delete(key)
         else:
             raise Exception('The specified key does not exist')
 
     def search(self, key):
-        node = self.singly_linked_list.search(key)
+        node = self.singly_linked_circular_list.search(key)
 
         return node.value if node else None
