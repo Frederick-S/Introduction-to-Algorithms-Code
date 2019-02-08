6fc18789720b36ae3d7e6acd9aa44e5b621f9898
Frederick-S <mao_xiaodan@hotmail.com>
2019-02-08 18:11:03 +0800

    Update set_union

--- src/chapter_10/set_union.py
+++ src/chapter_10/set_union.py
@@ -1,11 +1,5 @@
-from .singly_linked_list import SinglyLinkedList
-
-
 def set_union(s1, s2):
-    new_set = SinglyLinkedList()
-    new_set.head = s1.head
-    new_set.tail = s1.tail
-    new_set.tail.next = s2.head
-    new_set.tail = s2.tail
+    s1.tail.next = s2.head
+    s1.tail = s2.tail
 
-    return new_set
+    return s1
